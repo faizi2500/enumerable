2e9b7f43a8ffa4b714d975449847c01675fa0dbf
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-05 13:05:10 +0500

    Create class and initialize it in list.rb file

---
+++ list.rb
@@ -0,0 +1,14 @@
+require_relative 'enumerable'
+class MyList
+  
+  include MyEnumerable
+    
+  def initialize list
+    @list=list
+  end
+
+  def each
+    @list.each {|item| yield item}  
+  end
+  
+end
